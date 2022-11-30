bb5ec538e8ec1945cff2af119229c6d9c139ff69
Sxcram02 <ms2d0v4@gmail.com>
2022-11-30 19:14:58 +0100

    nuevo

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container")
+const toogle = document.querySelector(".toogle")
+container.addEventListener("click", () => {
+    container.classList.toggle("active")
+    toggle.classList.toggle("active")
+})
